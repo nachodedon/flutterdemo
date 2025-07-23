daca53a9f972eb97581988cb0a1bd5476b096d09
samuelorenzy@gmail.com <samuelorenzy@gmail.com>
2025-07-23 09:18:38 +0100

    Learnt variables

---
+++ flyboy.dart
@@ -0,0 +1,37 @@
+void main() {
+  // var address = 'Area1';
+  // var numberInClass = 5;
+
+  // print(address);
+  // String subject = "my dad is a government worker and a level 10 worker";
+  // print(subject);
+  // var aboutMe =
+  //     ''' My class has seven students and we start by 3pm i crom from $address''';
+  // print(aboutMe);
+  // int noOfStudents = 6;
+  // print(numberOfStudents);
+
+  // bool amHere = false;
+  // var isHere = true;
+  // final Myname = 'Samuel';
+
+  // String name = 'My name is Samuel';
+  // String location = ' i live in area1';
+  // print(name + location);
+
+  // String name = 'My name is Samuel';
+  // String location = '$name i live in area1';
+
+  // String name = 'My name is Praise';
+  // String location = ' and i live in Area1';
+  // print(name + location);
+
+  // String name = 'My name is Praise';
+  // String location = "$name and i live in Area1";
+  // print(location);
+
+  // String name = 'My name is Nacho';
+  // String location = ' and i live in Area1';
+  // String school = ' and i was a student of IMSU';
+  // print(name + location + school);
+}
